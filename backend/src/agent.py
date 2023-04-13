1ac9b19ce74fc57daaeac6b464b32fddbf0be522
Tomaz Bratanic <bratanic.tomaz@gmail.com>
2023-04-13 17:20:16 +0200

    support also text-davinci

--- backend/src/agent.py
+++ backend/src/agent.py
@@ -1,6 +1,7 @@
 from langchain.agents.agent import AgentExecutor
 from langchain.agents.tools import Tool
 from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.agents import initialize_agent
 from langchain.agents import AgentType
@@ -18,7 +19,11 @@ class MovieAgent(AgentExecutor):
 
     @classmethod
     def initialize(cls, movie_graph, model_name, *args, **kwargs):
-        llm = ChatOpenAI(temperature=0, model_name=model_name)
+        if model_name in ['gpt-3.5-turbo', 'gpt-4']:
+            llm = ChatOpenAI(temperature=0, model_name=model_name)
+        else:
+            llm = OpenAI(temperature=0, model_name=model_name)
+
         memory = ConversationBufferMemory(
             memory_key="chat_history", return_messages=True)
 
